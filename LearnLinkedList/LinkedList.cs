d6460e34e712167acfd3e172ba2d2031283affdc
AbdelRadwan <21023979@student.ltuc.com>
2024-07-11 14:59:44 +0300

    Add project files.

---
+++ LearnLinkedList/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLinkedList
+{
+    public class LinkedList
+    {
+        public Node First { get; set; }
+
+        public void INsertFirst(int data)
+        {
+            //Create the node 
+            Node newNode = new Node();
+            //Put the data in the node 
+            newNode.Data = data;
+            //Put the old date in the next
+            newNode.Next = First;
+            //Make the first the new node
+            First = newNode;
+        }
+        public Node DeleteFirst()
+        {
+            //Assign The Temprory Variable
+            Node temp = First;
+            //Assign the next head
+            First = First.Next;
+            return temp;
+        }
+        public void DisplayList()
+        {
+            Console.WriteLine("Iterating thrw list...");
+            Node current = First;
+            while (current != null)
+            {
+                current.DesplayData();
+                current = current.Next;
+            }
+        }
+
+        public void InsertLast(int data)
+        {
+            Node current = First;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            Node newNode = new Node();
+            newNode.Data = data;
+            current.Next = newNode; 
+
+        }
+    }
+}
---
+++ LearnLinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LearnLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Node nodeA = new Node();
+            //nodeA.Data = 1;
+
+            //Node nodeB = new Node();
+            //nodeB.Data = 2;
+
+            //Node nodeC = new Node();
+            //nodeC.Data = 3;
+
+            //Node nodeD = new Node();
+            //nodeD.Data = 4;
+
+            //nodeA.Next = nodeB;
+            //nodeB.Next = nodeC;
+            //nodeC.Next = nodeD;
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.INsertFirst(1);
+            linkedList.INsertFirst(2);
+            linkedList.INsertFirst(3);
+            linkedList.INsertFirst(4);
+
+            linkedList.DeleteFirst();
+
+            linkedList.InsertLast(665);
+
+            linkedList.DisplayList();
+            
+        }
+    }
+}
